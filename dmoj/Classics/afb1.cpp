7b5af5b400a8e942c0a964f9debf701de47b1f25
UserBlackBox <userblackbox@tutanota.com>
2021-01-23 09:47:55 -0500

    add afb1.cpp

---
+++ dmoj/Classics/afb1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int nums[n];
+    for(int i=0;i<n;i++){
+      cin >> nums[i];
+    }
+    sort(nums,nums+n);
+    for(int i=0;i<n;i++){
+      cout << nums[i] << endl;
+    }
+    return 0;
+}
+
